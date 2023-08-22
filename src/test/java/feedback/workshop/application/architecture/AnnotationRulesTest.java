928966af6da24d31cf96bb78ef7d1f61a4f857b7
jlasoc1 <jlasoc1@gmail.com>
2023-08-22 18:21:54 -0400

    refactors annotation rules test

---
+++ src/test/java/feedback/workshop/application/architecture/AnnotationRulesTest.java
@@ -0,0 +1,39 @@
+package feedback.workshop.application.architecture;
+
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchIgnore;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.junit.ArchUnitRunner;
+import com.tngtech.archunit.lang.ArchRule;
+import org.junit.runner.RunWith;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.RestController;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+
+@RunWith(ArchUnitRunner.class)
+@AnalyzeClasses(packages = "feedback.workshop.application")
+public class AnnotationRulesTest {
+
+
+  @ArchTest
+  public ArchRule components_should_be_located_in_infrastructure = classes()
+      .that().areAnnotatedWith(Component.class)
+      .should().resideInAPackage("..infrastructure..");
+
+  @ArchTest
+  public ArchRule repositories_should_be_located_in_the_domain = classes()
+      .that().areAnnotatedWith(Repository.class)
+      .should().resideInAPackage("..domain..");
+
+  @ArchTest
+  public static ArchRule controller_should_be_located_in_infrastructure = classes()
+      .that().areAnnotatedWith(RestController.class)
+      .should().resideInAPackage("..infrastructure..");
+
+  @ArchIgnore
+  @ArchTest
+  public static ArchRule services_should_located_in_infrastructure = classes()
+      .that().areAnnotatedWith(Service.class)
+      .should().resideInAPackage("..service..");
+}
